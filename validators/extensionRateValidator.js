8d170756cfa8a61d494af9f9d889cadb9d64f129
Nischal Shrestha <er.nischalshrestha@gmail.com>
2020-09-14 10:46:22 +0545

    extension-rate joi validation update

--- validators/extensionRateValidator.js
+++ validators/extensionRateValidator.js
@@ -2,11 +2,13 @@ const Joi = require('@hapi/joi');
 
 const create = Joi.object({
   name: Joi.string().label('Name').min(1).required(),
-  price: Joi.string().label('Price').min(1).required(),
+  price: Joi.number().label('Price').min(0).required(),
+  currency: Joi.string().label('Currency').min(1).required(),
 });
 const update = Joi.object({
   name: Joi.string().label('Name').min(1).optional(),
-  price: Joi.string().label('Price').min(1).optional(),
+  price: Joi.number().label('Price').min(0).optional(),
+  currency: Joi.string().label('Currency').min(1).optional(),
 });
 
 module.exports = {
