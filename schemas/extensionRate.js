d62791eb9d00f3437c302ccbeca828f4099c3cc0
Nischal Shrestha <er.nischalshrestha@gmail.com>
2020-09-14 10:43:23 +0545

    add current to guest-extension-rate

--- schemas/extensionRate.js
+++ schemas/extensionRate.js
@@ -2,5 +2,6 @@ const { createSchema } = require('.');
 
 module.exports = createSchema({
   name: { type: String, required: true },
-  price: { type: String, required: true },
+  price: { type: Number, required: true },
+  currency: { type: String, required: true },
 });
